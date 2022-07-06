59ce454f9b33c7e927aaf15d435059efa4567551
Алмаз Мустафин <a.n.mystafin@yandex.ru>
2022-07-06 15:02:46 +0500

    добавил участок кода для поворота отверстий

--- GeoAddin/Openings_Commands/OpeningGenerating.cs
+++ GeoAddin/Openings_Commands/OpeningGenerating.cs
@@ -15,6 +15,7 @@ using System.Linq;
 using System.Xml.Linq;
 using System.Windows;
 using RevitApplication = Autodesk.Revit.ApplicationServices.Application;
+using Revit.GeometryConversion;
 
 namespace GeoAddin
 {
@@ -56,7 +57,15 @@ namespace GeoAddin
                 List<Level> levels = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Levels).WhereElementIsNotElementType().Cast<Level>().ToList();
                 List<RevitLinkInstance> linkInstances = new FilteredElementCollector(doc).OfClass(typeof(RevitLinkInstance)).Cast<RevitLinkInstance>().ToList();
                 foreach (Level item in levels) {if (item.Name == win.LevelBox.Text) {level = item;}}
-                foreach (RevitLinkInstance item in linkInstances){if (item.Name == win.LinkInstance.Text){link = item; linkDoc = link.Document; } }
+                foreach (RevitLinkInstance item in linkInstances)
+                {
+                    foreach (Document document in app.Documents)
+                    {
+                        if ((item.Name == win.LinkInstance.Text) && (item.Name.Contains(document.Title))) { link = item; linkDoc = document; }
+                    }
+                     
+                }
+                string linkDocTitle = linkDoc.Title;
                 openingBoundOffset = Convert.ToDouble(win.BoundOffset.Text);
                 openingFunc = win.OpeningFunc.Text;
                 //
@@ -70,19 +79,19 @@ namespace GeoAddin
 
                 //Получение перекрытий и стен из связанного файла
                 List<Element> linkWalls = new FilteredElementCollector(linkDoc)
-                    .OfCategory(BuiltInCategory.OST_Walls)
+                    .OfClass(typeof(Wall))
                     .WhereElementIsNotElementType()
                     .Cast<Element>()
                     .Where(wall => wall.LookupParameter("Зависимость снизу").AsValueString() == level.Name)
                     .ToList();
                 List<Element> linkFloors = new FilteredElementCollector(linkDoc)
-                    .OfCategory(BuiltInCategory.OST_Floors)
+                    .OfClass(typeof(Floor))
                     .WhereElementIsNotElementType()
                     .Cast<Element>()
                     .Where(floor => floor.LookupParameter("Уровень").AsValueString() == level.Name)
                     .ToList();
                 List<Element> linkElements = linkFloors.Concat(linkWalls).ToList();
-
+                
                 //Получение воздуховодов, труб, кабельных лотков и коробов с активного вижа
                 List<Element> ductCurves = new FilteredElementCollector(doc, doc.ActiveView.Id)
                     .OfCategory(BuiltInCategory.OST_DuctCurves)
@@ -107,30 +116,56 @@ namespace GeoAddin
                 List<Element> mepElements = ductCurves.Concat(pipeCurves).Concat(cableTrays).Concat(conduits).ToList();
 
                 //Проверка пересечения элементов и получение точек вставки семейств
-                
-                foreach (Element mepElement in mepElements)
+                using (Transaction t = new Transaction(doc, "Генерация отверстий"))
                 {
-                    foreach (Element linkElement in linkElements)
+                    t.Start();
+                    foreach (Element mepElement in mepElements)
                     {
-                       Solid solid = GetSolidOfElement(linkElement);
-                       Line line = GetCenterLineOfElement(mepElement);
-                       SolidCurveIntersection curves =  solid.IntersectWithCurve(line, new SolidCurveIntersectionOptions());
-                       using (Transaction t = new Transaction(doc, "Генерация отверстий"))
+                        foreach (Element linkElement in linkElements)
                         {
-                            t.Start();
-                            foreach (Line curve in curves)
+                            Solid solid = GetSolidOfElement(linkElement);
+                            Line line = GetCenterLineOfElement(mepElement);
+                            SolidCurveIntersection intersectionCurves = solid.IntersectWithCurve(line, new SolidCurveIntersectionOptions());
+                            PlanarFace face = (PlanarFace)solid.Faces.get_Item(0);
+                            XYZ normalFace = face.FaceNormal;
+                            
+                            if (intersectionCurves.Count() > 0)
                             {
+                                /*var x =  (solid.ToProtoType().BoundingBox.Intersection(solidLine.ToProtoType().BoundingBox).MaxPoint.X + solid.ToProtoType().BoundingBox.Intersection(solidLine.ToProtoType().BoundingBox).MinPoint.X) / 2;
+                                double y = (solid.ToProtoType().BoundingBox.Intersection(line.ToProtoType().BoundingBox).MaxPoint.Y + solid.ToProtoType().BoundingBox.Intersection(line.ToProtoType().BoundingBox).MinPoint.Y) / 2;
+                                double z = (solid.ToProtoType().BoundingBox.Intersection(line.ToProtoType().BoundingBox).MaxPoint.Z + solid.ToProtoType().BoundingBox.Intersection(line.ToProtoType().BoundingBox).MinPoint.Z) / 2;*/
+                                /*XYZ locationPoint = new XYZ(x, y, z);
+                                XYZ directionPoint = solid.ToProtoType().Faces.First().SurfaceGeometry().TangentAtUParameter(solid.ToProtoType().Faces.First().SurfaceGeometry().UVParameterAtPoint(locationPoint.ToPoint()).U, solid.ToProtoType().Faces.First().SurfaceGeometry().UVParameterAtPoint(locationPoint.ToPoint()).V).ToXyz();
+                                */
+                                Line intersectionLine = intersectionCurves.First() as Line;
+                                double x = (intersectionLine.GetEndPoint(0).X + intersectionLine.GetEndPoint(1).X) / 2;
+                                double y = (intersectionLine.GetEndPoint(0).Y + intersectionLine.GetEndPoint(1).Y) / 2;
+                                double z = (intersectionLine.GetEndPoint(0).Z + intersectionLine.GetEndPoint(1).Z) / 2;
+                                XYZ locationPoint = new XYZ(x, y, z);
+                                Transform transform = Transform.CreateRotationAtPoint(new XYZ(0, 0, 1), (Math.PI * 90) / 180, locationPoint);
+                                XYZ transformedNormal = transform.OfVector(normalFace);
+                                Line axis = Line.CreateUnbound(locationPoint, transformedNormal);
 
-                                doc.Create.NewFamilyInstance(curve.Origin, openingFamilySymbol[0], doc.ActiveView);
+                                doc.Create.NewFamilyInstance(locationPoint, openingFamilySymbol[0], line.Direction, null, Autodesk.Revit.DB.Structure.StructuralType.NonStructural);
+                                List<ElementId> openingIds = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_DataDevices).WhereElementIsNotElementType().Select(el => el.Id).ToList();
+                                ElementTransformUtils.RotateElements(doc, openingIds, axis, (Math.PI* 90)/180);
 
 
                             }
-                            t.Commit();
                         }
-                        
                     }
-                }
+                    
+                    t.Commit();
 
+
+                }
+                /*using (Transaction t = new Transaction(doc, "Образмеривание отверстий"))
+                {
+                    t.Start();
+                    List<Element> openings = new FilteredElementCollector
+
+                    t.Commit();
+                }*/
             }
 
 
@@ -139,17 +174,15 @@ namespace GeoAddin
         private static Solid GetSolidOfElement(Element element)
         {
             Solid elementSolid = null;
-            GeometryElement geoElement = element.get_Geometry(new Options());
+            Options geomOptions = new Options();
+            geomOptions.ComputeReferences = true;
+            GeometryElement geoElement = element.get_Geometry(geomOptions);
             foreach (GeometryObject geoObject in geoElement)
             {
-                GeometryInstance instance = geoObject as GeometryInstance;
-                if (instance != null)
-                {
-                    foreach (GeometryObject instObj in instance.GetInstanceGeometry())
-                    {
-                        elementSolid = instObj as Solid;
-                    }
-                }
+               {
+                 elementSolid = geoObject as Solid;
+               }
+              
             }
 
             return elementSolid;
@@ -157,25 +190,13 @@ namespace GeoAddin
         private static Line GetCenterLineOfElement(Element element)
         {
             Line elementLine = null;
-            GeometryElement geoElement = element.get_Geometry(new Options());
-            foreach (GeometryObject geoObject in geoElement)
+            LocationCurve locationCurve =  element.Location as LocationCurve;
+            if (locationCurve != null)
             {
-                GeometryInstance instance = geoObject as GeometryInstance;
-                if (instance != null)
-                {
-                    foreach (GeometryObject instObj in instance.GetInstanceGeometry())
-                    {
-                        int insId = instObj.GraphicsStyleId.IntegerValue;
-                        if (doc.GetElement(new ElementId(insId)).Name == "Осевая линия" )
-                        {
-                            elementLine = instObj as Line;
-                        }
-
-                        
-                    }
-                }
+                Curve curve = locationCurve.Curve;
+                elementLine = curve as Line;
             }
-
+           
             return elementLine;
         }
     }
